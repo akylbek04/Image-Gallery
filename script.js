b64eee4100783849dfe099769a7564c973b01738
Akylbek04 <zamirovakylbek920@gmail.com>
2023-07-05 19:50:27 +0200

    modal functionality added

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-
-// const galleryImg = document.querySelector(".image-gallery");
-// document.addEventListener("DOMContentLoaded", () => {
-//     fetchFhotos();
-// });
-
-
-// const fetchFhotos = async()=> {
-//     const res = await fetch("https://api.unsplash.com/photos/?client_id=76lSVJPvbAXlVHOUl9Pnn4TQmrMWtjqXknZPgpAUEUI");
-//     const data = await res.json();
-//     console.log(data);
-//     renderPhotos(data);
-
-// }
-
-// const renderPhotos = (imgs) => {
-//     imgs.forEach( el => {
-//         const div = document.createElement("div");
-//         div.className = "image-item";
-//         const img = document.createElement("img");
-//         img.src = el.urls.regular;
-//         div.appendChild(img);
-//         galleryImg.appendChild(div)
-//         el.addEventListener('click', () => {
-//             showImage();
-//         })
-//     })
-// }
-
-
 const imageGallery = document.querySelector('.image-gallery')
 document.addEventListener('DOMContentLoaded', function(){
     fetchPhotos()
@@ -58,4 +28,33 @@ const renderImages = (imageArray) => {
     });
     
 }
+const showImage = (imageObject) => {
+    
+    const div = document.createElement('div');
+    div.className = 'modal-container'
+       
+    div.innerHTML = `
+        <img src=${imageObject.urls.regular}/>
+        <p>Author: ${imageObject.user.name}</p>
+        <p>Description: ${imageObject.alt_description}</p>
+        <button class='close-button' onclick=closeModal()>Close</button>
+    `
+    document.body.appendChild(div)
+// var myDiv = document.createElement('div');
+// myDiv.className = 'modal-container'
+// var closeButton = document.createElement('button');
+// closeButton.innerHTML = 'Close';
+// closeButton.addEventListener('click', function() {
+//   myDiv.style.display = 'none';
+// });
 
+// myDiv.appendChild(closeButton);
+// document.body.appendChild(myDiv)
+}
+
+const closeModal = () => {
+    const modalContainer = document.querySelector('.modal-container')
+    if(modalContainer){
+        modalContainer.remove()
+    }
+}
